ab882e2ef650f6989366c78da9dd5befb1e2b0c7
AndreMotta25 <andremotta16041998@gmail.com>
2023-01-22 16:45:16 -0300

    creating the error middleware

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+
+import './shared/container';
+
+import ErrorField from './modules/errors/ErrorField';
+import router from './routes';
+
+const app = express();
+app.use(express.json());
+
+app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof ErrorField) {
+      return response.status(err.statusCode).json({
+        errors: [{ value: err.value, msg: err.msg, param: err.param }],
+      });
+    }
+
+    return response.status(500).json({ message: err.message });
+  }
+);
+
+app.listen(3333, () => {
+  console.log('Server is runnig');
+});

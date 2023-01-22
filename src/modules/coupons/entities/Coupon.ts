e8625da3deaad60c5110c48f7eda2723eba3190d
AndreMotta25 <andremotta16041998@gmail.com>
2023-01-22 16:47:56 -0300

    feat: making the coupon entity

---
+++ src/modules/coupons/entities/Coupon.ts
@@ -0,0 +1,34 @@
+import { Column, PrimaryColumn, Entity, CreateDateColumn } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+
+@Entity('Coupons')
+class Coupon {
+  @PrimaryColumn()
+  id: string;
+
+  @Column('varchar', { length: 5 })
+  code: string;
+
+  @Column('decimal', { precision: 18, scale: 2 })
+  value: number;
+
+  @Column()
+  amount: number;
+
+  @Column()
+  expire_at: Date;
+
+  @CreateDateColumn({ type: 'timestamp with time zone' })
+  created_at: Date;
+
+  @Column({ default: true })
+  valid: boolean;
+
+  @Column('decimal', { precision: 18, scale: 2, default: 10.0 })
+  minimumValue: number;
+
+  constructor() {
+    this.id = uuidv4();
+  }
+}
+export default Coupon;

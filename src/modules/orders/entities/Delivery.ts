bc48100e5e798745a51c922af4675734b153e6c4
AndreMotta25 <andremotta16041998@gmail.com>
2023-04-17 17:38:23 -0300

    ✨ feat: delivery entity

---
+++ src/modules/orders/entities/Delivery.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
+import { v4 } from 'uuid';
+
+import { Order } from './Order';
+
+@Entity('Deliverys')
+class Delivery {
+  @PrimaryColumn()
+  id: string;
+
+  @OneToOne(() => Order, { onDelete: 'CASCADE', nullable: false })
+  @JoinColumn()
+  order: Order;
+
+  @Column({ nullable: false })
+  adress: string;
+
+  // retirar depois
+  @Column({ default: 'A enviar' })
+  status: string;
+
+  @Column({ default: false })
+  send: boolean;
+
+  constructor() {
+    this.id = v4();
+  }
+}
+
+export { Delivery };

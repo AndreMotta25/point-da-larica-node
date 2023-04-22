0d35fb7c67aa9dc2cc1a008a5d84798bf460df37
AndreMotta25 <andremotta16041998@gmail.com>
2023-04-22 17:06:49 -0300

    ✨ feat: make dependency injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,16 @@
-import { container } from 'tsyringe';
+import { container, Lifecycle } from 'tsyringe';
 
-import ICodeGenerator from '../../modules/coupons/providers/interfaces/ICodeGenerator';
+import { IDeliveryRepository } from '@modules/orders/repositories/IDeliveryRepository';
+import { DeliveryRepository } from '@modules/orders/repositories/implementations/DeliveryRepository';
+import { OrderListRepository } from '@modules/orders/repositories/implementations/OrderListRepository';
+import { OrderRepository } from '@modules/orders/repositories/implementations/OrderRepository';
+import { ProductRepository } from '@modules/orders/repositories/implementations/ProductRepository';
+import { IOrderListRepository } from '@modules/orders/repositories/IOrderListRepository';
+import { IOrderRepository } from '@modules/orders/repositories/IOrderRepository';
+import { IProductRepository } from '@modules/orders/repositories/IProductRepository';
+
 import CodeGenerator from '../../modules/coupons/providers/implementations/CodeGenerator';
+import ICodeGenerator from '../../modules/coupons/providers/interfaces/ICodeGenerator';
 import ICouponRepository from '../../modules/coupons/repositories/ICouponRepository';
 import CouponRepository from '../../modules/coupons/repositories/implementations/CouponRepository';
 
@@ -9,4 +18,23 @@ container.register<ICodeGenerator>('CodeGenerator', CodeGenerator);
 container.registerSingleton<ICouponRepository>(
   'CouponRepository',
   CouponRepository
+);
+container.registerSingleton<IProductRepository>(
+  'ProductRepository',
+  ProductRepository
+);
+
+container.registerSingleton<IOrderRepository>(
+  'OrderRepository',
+  OrderRepository
+);
+
+container.registerSingleton<IOrderListRepository>(
+  'OrderListRepository',
+  OrderListRepository
+);
+
+container.registerSingleton<IDeliveryRepository>(
+  'DeliveryRepository',
+  DeliveryRepository
 );

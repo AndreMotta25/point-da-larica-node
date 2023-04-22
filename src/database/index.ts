4597fb81d4efc1eb72799a64d11971e9e23715ef
AndreMotta25 <andremotta16041998@gmail.com>
2023-04-22 17:19:38 -0300

    ✨ chore: docker connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,10 +3,13 @@ import { DataSource } from 'typeorm';
 export default new DataSource({
   type: 'postgres',
   host: 'localhost',
-  username: 'postgres',
-  password: 'desenhos1@',
+  username: 'docker',
+  password: 'docker',
   database: 'pointDaLarica',
-  port: 5432,
-  entities: ['./src/modules/coupons/entities/*.ts'],
+  port: 9000,
+  entities: [
+    './src/modules/coupons/entities/*.ts',
+    './src/modules/orders/entities/*.ts',
+  ],
   migrations: ['./src/database/migrations/*.ts'],
 });

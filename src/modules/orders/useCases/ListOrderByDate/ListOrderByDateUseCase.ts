614c7e9de7cd71a8140d74366b05d3b56dedf6ec
AndreMotta25 <andremotta16041998@gmail.com>
2023-05-02 17:30:34 -0300

    ♻️ refactor: changing the way to do pagination

--- src/modules/orders/useCases/ListOrderByDate/ListOrderByDateUseCase.ts
+++ src/modules/orders/useCases/ListOrderByDate/ListOrderByDateUseCase.ts
@@ -5,10 +5,10 @@ import { IOrderRepository } from '@modules/orders/repositories/IOrderRepository'
 
 import { IOrderResponseDTO } from './IOrderResponseDTO';
 
-interface IListOrderDTO {
-  date: string;
-  minDate: string;
-  maxDate: string;
+export interface IListOrderDTO {
+  date?: string;
+  minDate?: string;
+  maxDate?: string;
   limit: number;
   page: number;
 }
@@ -22,36 +22,15 @@ class ListOrderByDateUseCase {
   }
 
   async execute({ date, minDate, maxDate, limit, page }: IListOrderDTO) {
-    const orders = await this.repository.getOrders();
+    const orders = this.repository.getOrderByDate({
+      date,
+      minDate,
+      maxDate,
+      limit,
+      page,
+    });
 
-    const ordersByDates = orders
-      .filter(
-        (order) =>
-          (date &&
-            order.data_of_sale.toLocaleDateString() ===
-              new Date(`${date}T00:00`).toLocaleDateString()) ||
-          (minDate &&
-            maxDate &&
-            order.data_of_sale.toLocaleDateString() >=
-              new Date(`${minDate}T00:00`).toLocaleDateString() &&
-            order.data_of_sale.toLocaleDateString() <=
-              new Date(`${maxDate}T00:00`).toLocaleDateString()) ||
-          (!date && !minDate && !maxDate && order)
-      )
-      .map((order) => {
-        const orderDTO: IOrderResponseDTO = {
-          id: order.id,
-          date_of_sale: order.data_of_sale,
-          full_value: Number(order.full_value),
-          situation: order.canceled ? 'cancelado' : 'ativo',
-          discounted_value: Number(order.discounted_value),
-        };
-        return orderDTO;
-      });
-    if (limit && page)
-      return pagination<IOrderResponseDTO>(ordersByDates, limit)[page - 1];
-
-    return ordersByDates;
+    return orders;
   }
 }
 

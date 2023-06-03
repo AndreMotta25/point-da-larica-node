a07c73241aec4c4f7f285c38a1ba74be75eb1b7f
AndreMotta25 <andremotta16041998@gmail.com>
2023-06-03 17:00:57 -0300

    ✨ feat(employer/route): endpoint for assign roles

--- src/routes/employer.routes.ts
+++ src/routes/employer.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { hasPermission } from 'src/middleware/hasPermission';
 import { isAuthenticated } from 'src/middleware/isAuthenticated';
 
+import { AssignRolesController } from '@modules/users/useCases/AssignRoles/AssignRolesController';
 import { AuthenticateUserController } from '@modules/users/useCases/AuthenticateUser/AuthenticateUserController';
 import { CreateEmployerController } from '@modules/users/useCases/CreateEmployer/CreateEmployerController';
 
@@ -9,6 +10,7 @@ const employerRoutes = Router();
 
 const authenticateController = new AuthenticateUserController();
 const createController = new CreateEmployerController();
+const assignRoles = new AssignRolesController();
 
 employerRoutes.post(
   '/',
@@ -16,6 +18,7 @@ employerRoutes.post(
   hasPermission('create_user'),
   createController.handle
 );
+employerRoutes.post('/:id/assign_roles', isAuthenticated, assignRoles.handle);
 
 employerRoutes.post('/session', authenticateController.handle);
 

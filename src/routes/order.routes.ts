a2b72e259734aacf60b75b6f06f2c68bc1ba208c
AndreMotta25 <andremotta16041998@gmail.com>
2023-06-03 17:39:31 -0300

    ✨ feat(order/route): cancel a order

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+import { CancelOrderController } from '@modules/orders/useCases/CancelOrder/CancelOrderController';
 import { CreateOrderController } from '@modules/orders/useCases/CreateOrder/CreateOrderController';
 import { GetOrderController } from '@modules/orders/useCases/GetOrder/GetOrderController';
 import { ListByDeliveriesController } from '@modules/orders/useCases/ListByDeliveries/ListByDeliveriesController';
@@ -13,11 +14,13 @@ const listByDate = new ListOrderByDateController();
 const listByDeliveries = new ListByDeliveriesController();
 const getOrderController = new GetOrderController();
 const sendOrderController = new SendOrderController();
+const cancelOrderController = new CancelOrderController();
 
 orderRoutes.post('/', createOrderController.handler);
 orderRoutes.get('/date', listByDate.handler);
 orderRoutes.get('/deliveries', listByDeliveries.handler);
 orderRoutes.get('/:id', getOrderController.handler);
 orderRoutes.patch('/send/:id', sendOrderController.handle);
+orderRoutes.patch('/cancel/:id', cancelOrderController.handle);
 
 export { orderRoutes };

7eb8b648ea6be1444e5b28e9478eb58c7db66236
AndreMotta25 <andremotta16041998@gmail.com>
2023-06-12 13:48:16 -0300

    ♻️ refactor: calling the next middleware that will invalidate the token

--- src/modules/orders/useCases/CancelOrder/CancelOrderController.ts
+++ src/modules/orders/useCases/CancelOrder/CancelOrderController.ts
@@ -1,17 +1,17 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import { CancelOrderUseCase } from './CancelOrderUseCase';
 
 class CancelOrderController {
-  async handle(request: Request, response: Response) {
+  async handle(request: Request, response: Response, next: NextFunction) {
     const { id } = request.params;
     const cancelOrderUseCase = container.resolve(CancelOrderUseCase);
 
     const orderCanceled = await cancelOrderUseCase.execute(id);
-    return response.status(200).json(orderCanceled);
+
+    return next();
   }
 }
 
 export { CancelOrderController };
-// total discount

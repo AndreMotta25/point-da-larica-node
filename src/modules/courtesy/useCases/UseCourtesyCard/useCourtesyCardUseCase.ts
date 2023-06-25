5a3b02d1c233786ff43c9f9dca5e523fe3a67108
AndreMotta25 <andremotta16041998@gmail.com>
2023-06-25 15:42:29 -0300

    ♻️ refactor: improving the presentation of the data

--- src/modules/courtesy/useCases/UseCourtesyCard/useCourtesyCardUseCase.ts
+++ src/modules/courtesy/useCases/UseCourtesyCard/useCourtesyCardUseCase.ts
@@ -1,10 +1,7 @@
-import { container, inject, injectable } from 'tsyringe';
+import { inject, injectable } from 'tsyringe';
 
 import AppError from '@errors/AppError';
-import { IUseCourtesyCardRequest } from '@modules/courtesy/dtos/Request/IUseCourtesyCardRequest';
 import { ICourtesyCardRepository } from '@modules/courtesy/repositories/ICourtesyCardRepository';
-import { CreateOrderUseCase } from '@modules/orders/useCases/CreateOrder/CreateOrderUseCase';
-import { GetTotalUseCase } from '@modules/orders/useCases/GetTotal/GetTotalUseCase';
 
 @injectable()
 class useCourtesyCardUseCase {
@@ -20,36 +17,13 @@ class useCourtesyCardUseCase {
       throw new AppError('O cliente não tem credito na loja!', 400);
 
     if (courtesyCard.expiresIn < new Date())
-      throw new AppError(`A cortesia expirou em ${courtesyCard.expiresIn}`);
+      throw new AppError(
+        `A cortesia expirou em ${courtesyCard.expiresIn.toLocaleString()}`
+      );
 
     if (courtesyCard.value <= 0) throw new AppError('Sem saldo suficiente');
 
     return courtesyCard;
-    // const createOrderUseCase = container.resolve(CreateOrderUseCase);
-
-    /*
-        O courtesy vem para cá para podemos inserir no banco o quanto ficou faltando
-    */
-    // const order = await createOrderUseCase.execute({
-    //   adress,
-    //   isDelivery,
-    //   itens,
-    //   schedule,
-    //   coupon_code,
-    //   schedule_date,
-    //   courtesy: courtesyCard,
-    // });
-    // if (!order) throw new AppError('Erro ao processar o pagamento');
-
-    // reduz o credito em loja
-    // const creditValue = order.full_value - courtesyCard.value;
-
-    // return {
-    //   remaining_balance:
-    //     order.additionalPayment > 0 ? order.additionalPayment : 0,
-    //   finalized: !(order.additionalPayment > 0),
-    //   id_order: order.id,
-    // };
   }
 }
 

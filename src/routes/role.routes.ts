fc0b98067e4d91e2e3f5cbaba562193ec6ec6303
AndreMotta25 <andremotta16041998@gmail.com>
2023-06-29 14:07:49 -0300

    ✨ feat: get roles

--- src/routes/role.routes.ts
+++ src/routes/role.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
+import { hasPermission } from 'src/middleware/hasPermission';
 import { is } from 'src/middleware/is';
 import { isAuthenticated } from 'src/middleware/isAuthenticated';
 
 import { AssignPermitionsController } from '@modules/users/useCases/AssignPermitions/AssignPermitionsController';
 import { CreateRoleController } from '@modules/users/useCases/CreateRole/CreateRoleController';
+import { GetRolesController } from '@modules/users/useCases/GetRoles/GetRolesController';
 
 const roleRoutes = Router();
 
 const rolePostController = new CreateRoleController();
 const assignPermitionController = new AssignPermitionsController();
+const getRolesController = new GetRolesController();
 
 roleRoutes.post('/', isAuthenticated, is('admin'), rolePostController.handle);
 roleRoutes.post(
@@ -16,6 +19,12 @@ roleRoutes.post(
   isAuthenticated,
   is('admin'),
   assignPermitionController.handle
+);
+roleRoutes.get(
+  '/',
+  isAuthenticated,
+  hasPermission('get_role'),
+  getRolesController.handle
 );
 
 export { roleRoutes };

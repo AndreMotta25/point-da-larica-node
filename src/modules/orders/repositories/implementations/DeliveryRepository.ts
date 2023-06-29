41e4750e042c91da3bb692289578920b53212a5a
AndreMotta25 <andremotta16041998@gmail.com>
2023-06-28 23:35:27 -0300

    ✨ feat: add query Runner

--- src/modules/orders/repositories/implementations/DeliveryRepository.ts
+++ src/modules/orders/repositories/implementations/DeliveryRepository.ts
@@ -1,18 +1,24 @@
 import database from 'src/database';
+import { IQueryRunner } from 'src/database/transactions/QueryRunner/IQueryRunner';
+import { inject, injectable } from 'tsyringe';
 import { Repository } from 'typeorm';
 
 import { Delivery } from '@modules/orders/entities/Delivery';
 
 import { IDeliveryDTO, IDeliveryRepository } from '../IDeliveryRepository';
 
+@injectable()
 class DeliveryRepository implements IDeliveryRepository {
   private readonly repository: Repository<Delivery>;
+  private queryRunner: IQueryRunner;
 
-  constructor() {
+  constructor(@inject('QueryRunner') runner: IQueryRunner) {
     this.repository = database.getRepository(Delivery);
+    this.queryRunner = runner;
   }
   async save(delivery: Delivery): Promise<Delivery> {
     const deliveryUpdate = await this.repository.save(delivery);
+
     return deliveryUpdate;
   }
   async getOrderDelivery(id: string): Promise<Delivery | null> {
@@ -26,8 +32,11 @@ class DeliveryRepository implements IDeliveryRepository {
   }
 
   async create({ adress, order }: IDeliveryDTO): Promise<void> {
-    const delivery = this.repository.create({ adress, order });
-    await this.repository.save(delivery);
+    const runnerRepository = this.queryRunner.getRepository(Delivery);
+
+    const delivery = runnerRepository.create({ adress, order });
+
+    await runnerRepository.save(delivery);
   }
 }
 

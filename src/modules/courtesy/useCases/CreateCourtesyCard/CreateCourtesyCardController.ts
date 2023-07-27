4e7244b99a016e20cfeef22a52ea51d87d42dfea
AndreMotta25 <andremotta16041998@gmail.com>
2023-07-27 18:10:33 -0300

    ✨ feat(courtesy): using provider validator

--- src/modules/courtesy/useCases/CreateCourtesyCard/CreateCourtesyCardController.ts
+++ src/modules/courtesy/useCases/CreateCourtesyCard/CreateCourtesyCardController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from 'express';
+import { validator } from 'src/provider/resultValidator/implements/validator';
 import { container } from 'tsyringe';
 
 import { CreateCourtesyCardUseCase } from './CreateCourtesyCardUseCase';
 
 class CreateCourtesyCardController {
   async handle(request: Request, response: Response) {
+    const result = validator(request);
+
+    if (result.hasErrors())
+      return response.status(400).json({ errors: result.getErrors() });
+
     const { value, cpf, motivation } = request.body;
     const { id } = request.user;
 
--- src/modules/courtesy/useCases/GetCourtesyCard/GetCourtesyCardController.ts
+++ src/modules/courtesy/useCases/GetCourtesyCard/GetCourtesyCardController.ts
@@ -1,10 +1,16 @@
 import { Request, Response } from 'express';
+import { validator } from 'src/provider/resultValidator/implements/validator';
 import { container } from 'tsyringe';
 
 import { GetCourtesyCardUseCase } from './GetCourtesyCardUseCase';
 
 class GetCourtesyCardController {
   async handle(request: Request, response: Response) {
+    const result = validator(request);
+
+    if (result.hasErrors())
+      return response.status(400).json({ errors: result.getErrors() });
+
     const { code } = request.params;
 
     const getCourtesyCardUseCase = container.resolve(GetCourtesyCardUseCase);

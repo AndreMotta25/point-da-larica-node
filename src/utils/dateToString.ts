91e7cd8a96b6d44eed2ea0253f475cce9bb6a823
AndreMotta25 <andremotta16041998@gmail.com>
2023-08-20 18:41:25 -0300

    ✨ feat: using jsdoc

--- src/utils/dateToString.ts
+++ src/utils/dateToString.ts
@@ -1,3 +1,10 @@
+/**
+ *
+ * Vai retornar só a primeira parte de uma data
+ * @param {date} date Ex: Ex: 2023-04-20T00:00:00Z
+ * @returns {string} Ex: 2023-04-20
+ *
+ */
 const dateToString = (date: Date) => {
   const dateNow = date.toISOString().split('T')[0];
   return dateNow;
--- src/utils/utcToLocal.ts
+++ src/utils/utcToLocal.ts
@@ -1,19 +1,23 @@
-/**
- *
- * @param date
- * @param locale
- * @returns string
- *
- * Vai retirar a data do formato utc, deixando numa string numa hora local.
- */
+import { toFormatISO } from './toFormatISO';
+
 type IOptions = {
   locale: Intl.LocalesArgument;
   config: Intl.DateTimeFormatOptions;
 };
-const utcToLocal = (date: Date, options: IOptions) => {
+
+/**
+ * Retira a data do formato utc e retorna a data como string no horario local
+ * @param {date} date Ex: 2023-04-20T00:00:00Z
+ * @param locale
+ * @returns {string} Ex: 2023-04-20T21:00:00
+ *
+ * Vai retirar a data do formato utc, deixando numa string numa hora local.
+ */
+
+const utcToLocal = (date: Date, options: IOptions): string => {
   const dateString = date.toLocaleString(options.locale, options.config);
   const [fullYear, time] = dateString.split(' ');
-  const fullYearIso = fullYear.split('/').reverse().join('-');
+  const fullYearIso = toFormatISO(fullYear);
   return `${fullYearIso}T${time}`;
 };
 
